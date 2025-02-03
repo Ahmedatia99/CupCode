d5682177564caed63f50163bcc794f847360b10d
Ahmedatia99 <atyaa629@gmail.com>
2025-02-04 01:34:18 +0200

    add Comment section && swiping

---
+++ src/components/Comments.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Swiping from "./Swiping";
+
+const Comments = () => {
+  return (
+    <main className="max-sm:px-2 px-5 h-screen py-10 max-sm:py-5  flex justify-center bg-lighting overflow-hidden">
+      <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-5 max-md:grid-cols-1 items-center container mx-auto h-full">
+        {/*  */}
+        <div className="flex flex-col justify-center relative w-full h-full">
+          <div className="flex justify-end">
+            <img src="/images/quote.png" className="aspect-auto max-sm:w-20 max-md:w-18 " />
+          </div>
+          <h1 className="text-5xl max-sm:text-3xl max-md:text-4xl text-darkness tracking-tight leading-14 max-md:leading-10 max-sm:leading-8 font-semibold">
+            Student’s Stories! Some Awesome Comments By Our
+            <span className="font-courgette font-medium"> Students!</span>
+          </h1>
+          <p className="text-gray-500 mt-5">
+            Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam
+            tempus. Volutpat tortor tincidunt egestas sit risus donec.
+          </p>
+          <div className="flex justify-center mt-10 relative max-sm:hidden ">
+            <img src="/images/abstract/vector-7.png" className="max-md:rotate-180 max-md:absolute max-md:right-0" />
+            <img
+              src="/images/abstract/dots-1.png"
+              className="swiping aspect-auto w-auto h-14 absolute bottom-0 max-md:hidden"
+            />
+          </div>
+        </div>
+        <div className="">
+          <img src="/images/abstract/cover-comment.png" className="absolute" />
+          <Swiping />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Comments;
---
+++ src/components/Swiping.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { comments } from "../constants";
+import { FaStar } from "react-icons/fa6";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const Swiping = () => {
+  const pagination = {
+    clickable: true,
+    renderBullet: (className) => 
+      `<span class="${className}"></span>`,
+  };
+
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={40}
+      pagination={pagination}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      {comments.map((item) => (
+        <SwiperSlide
+          className="!flex justify-center max-sm:flex-col items-center"
+          key={item.id}
+        >
+          <div className="w-fit relative comment">
+            <img
+              src={item.img}
+              alt="comment"
+              className="w-48 h-auto rounded-xl mt-10 max-sm:mt-0"
+            />
+          </div>
+          <div className="bg-white h-50 max-sm:h-46 text-darkness max-w-sm relative rounded-2xl mt-10 max-sm:mt-0">
+            <h1 className="text-3xl max-sm:text-xl font-medium p-5 max-sm:p-3 pb-2">
+              {item.name}
+            </h1>
+            <div className="flex gap-2 px-5 max-sm:px-3 pt-2">
+              {Array.from({ length: 4 }, (_, index) => (
+                <FaStar key={index} size={20} className="text-shiny" />
+              ))}
+            </div>
+            <p className="px-5 max-sm:px-3 pt-3 text-gray-500 max-sm:text-xs">
+              {item.comment}
+            </p>
+            <img
+              src="/images/abstract/curve.png"
+              className="w-full absolute bottom-0 rounded-2xl"
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Swiping;
