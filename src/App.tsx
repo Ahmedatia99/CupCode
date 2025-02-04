c8d308b6119d8d72854ea4199b9bad98dd868486
Ahmedatia99 <atyaa629@gmail.com>
2025-02-04 07:19:32 +0200

    add SWiper-react and images

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import KnowUs from "./components/KnowUs";
 import Questions from "./components/Questions";
+import Comments from './components/Comments';
+import Courses from './components/Courses';
+
 function App() {
   return (
-    <div className=" overflow-hidden">
-      <div className="hero-bg w-full overflow-hidden py-10 max-sm:py-6 px-5">
+    <div className="snap-y snap-mandatory h-screen">
+      <div className="hero-bg w-full overflow-hidden py-10 max-sm:py-6 px-5 snap-start">
         <Navbar />
         <Hero />
       </div>
+      <Courses />
       <KnowUs />
       <Questions />
+      <Comments />
     </div>
   );
 }
